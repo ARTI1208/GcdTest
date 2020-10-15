eae500524489bcbe01528efc098fb77596a5f682
ARTI1208 <leonardo906@mail.ru>
2020-10-15 22:56:03 +0300

    Test function templates

--- src/test/java/root/gcd/GCDTest.java
+++ src/test/java/root/gcd/GCDTest.java
@@ -7,6 +7,36 @@ import static org.junit.Assert.*;
 public class GCDTest {
 
     @Test
-    public void gcd() {
+    public void positiveArgs() {
     }
+
+    @Test
+    public void negativeArgs() {
+    }
+
+    @Test
+    public void zeroArgs() {
+    }
+
+    @Test
+    public void coprimeIntegers() {
+    }
+
+    @Test
+    public void equalArgs() {
+    }
+
+    @Test
+    public void argDividesArg() {
+    }
+
+    @Test
+    public void notEqualArgsMaxGcd() {
+    }
+
+    @Test
+    public void borderArgs() {
+    }
+
+
 }
