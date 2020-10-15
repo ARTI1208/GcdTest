294de3e73033aebf21e81c1bb884da2c4d98859e
asmaksimov3 <asmaksimov_3@edu.hse.ru>
2020-10-16 00:28:48 +0300

    add some methods implements

--- src/test/java/root/gcd/GCDTest.java
+++ src/test/java/root/gcd/GCDTest.java
@@ -17,30 +17,47 @@ public class GCDTest {
 
     @Test
     public void negativeArgs() {
+
     }
 
     @Test
     public void zeroArgs() {
+        GCD gcd=new GCD();
+        int answer1=gcd.gcd(0,0);
+        int answer2=gcd.gcd(10,0);
+        int answer3=gcd.gcd(0,7);
+        Assert.assertTrue(answer1==0 && answer2==10 && answer3==7);
     }
 
     @Test
     public void coprimeIntegers() {
+        GCD gcd=new GCD();
+        int answer=gcd.gcd(15,8);
+        Assert.assertEquals(answer,1);
     }
 
     @Test
     public void equalArgs() {
+        GCD gcd=new GCD();
+        int answer=gcd.gcd(7,7);
+        Assert.assertEquals(answer,7);
     }
 
     @Test
     public void argDividesArg() {
+
     }
 
     @Test
     public void notEqualArgsMaxGcd() {
+        GCD gcd=new GCD();
+        int res=gcd.gcd(45,60);
+        Assert.assertEquals(res,15);
     }
 
     @Test
     public void borderArgs() {
+
     }
 
 
